b6d4df4c0a4734c5b8deb4b728284cfcaf1dff4d
merrygold <merrygold643@gmail.com>
2022-09-03 10:16:00 +0500

    pakage.json changes

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,12 +1,11 @@
 
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useEffect, useState , useContext } from 'react'
 import { FiArrowUpRight } from 'react-icons/fi'
 import { AiOutlineDown } from 'react-icons/ai'
 import { HiOutlineDotsVertical } from 'react-icons/hi'
 import ethLogo from '../assets/eth.png'
 import uniswapLogo from '../assets/uniswap.png'
-import { useContext } from 'react'
 import { TransactionContext } from '../context/TransactionContext'
 import { client } from '../lib/sanityClient'
 
